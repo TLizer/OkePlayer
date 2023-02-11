ea9b438769fbd54524cca9b892c2c556f0e39734
Tomasz Lizer <tomasz.lizer@brightinventions.pl>
2023-02-11 20:05:35 +0100

    Add tests to project

---
+++ OkePlayerTests/OkePlayerTests.swift
@@ -0,0 +1,14 @@
+//
+//  OkePlayerTests.swift
+//  OkePlayerTests
+//
+//  Created by Tomasz Lizer on 11/02/2023.
+//
+
+import XCTest
+
+final class OkePlayerTests: XCTestCase {
+    func testExample() throws {
+        XCTAssertTrue(true)
+    }
+}
